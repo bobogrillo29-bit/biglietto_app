d5ef356b33691c3470ebf63c934ff8d17187a970
bobogrillo29-bit <bobogrillo29@gmail.com>
2025-09-26 15:10:07 +0200

    Primo commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const ImageApp());
+
+class ImageApp extends StatelessWidget {
+  const ImageApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ImageScreen(),
+    );
+  }
+}
+
+class ImageScreen extends StatelessWidget {
+  const ImageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 168, 66, 66),
+      body: Center(
+        child: ClipRRect(
+          child: Image(image: const AssetImage('assets/images/biglietto.jpg')),
+        ),
+      ),
+    );
+  }
+}
